606ff30613f53a6f03dee710aae5716df0ec05fd
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-01-10 02:33:23 +0900

    Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log('Hello World');
+
+/* 
+STEP 2
+When a function (getComputerChoice) is called 
+Return a random number greater than or equal to 0 and less than 1
+Exchange the number to rock/paper/scissors
+*/
+
+let randomNumber = Math.random() * 100;
+
+function getComputerChoice() {
+    if (randomNumber <= 33) {
+        return 'rock';
+    } else if (randomNumber > 66) {
+        return 'paper';
+    } else 
+        return 'scissors';
+}
+
+// SWITCH VERSION:
+// function getComputerChoice() {
+//     switch (false) {
+//     case randomNumber <= 33:
+//         return 'rock';
+//         break;
+//     case randomNumber > 66:
+//         return 'paper';
+//         break;
+//     default:
+//         return 'scissors';
+//         break;
+//     }
+// }
+
+console.log(randomNumber);
+console.log(getComputerChoice());
+
+
