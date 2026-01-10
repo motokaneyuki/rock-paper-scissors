22a9147874ae21f1347f029f7c77de4a329cdbe8
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-01-10 13:24:12 +0900

    Add increments to score when winning

--- script.js
+++ script.js
@@ -55,22 +55,28 @@ let humanScore = 0;
 function playRound (humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'paper') {
         console.log('You lose! Paper beats Rock');
+        computerScore++;
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
         console.log('You win! Rock beats scissors');
+        humanScore++;
     } else if (humanChoice === 'rock' && computerChoice === 'rock') {
         console.log('It\'s a tie');
     } else if (humanChoice === 'paper' && computerChoice === 'paper') {
         console.log('It\'s a tie');
     } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
         console.log('You lose! Scissors beat Paper');
+        computerScore++;
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
         console.log('You win! Paper beats rock');
+        humanScore++;
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         console.log('You win! Scissors beat Paper');
+        humanScore++;
     } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
         console.log('It\'s a tie');
     } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
         console.log('You lose! Rock beats Scissors');
+        computerScore++;
     } 
 }
 
@@ -78,3 +84,5 @@ let humanSelection = getHumanChoice();
 let computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+console.log('Your Score: ' + humanScore);
+console.log('Computer Score: ' + computerScore);
