961ecbba15c4d7ddf5844b90f225898808075bee
Yuki Motokane <216834584+motokaneyuki@users.noreply.github.com>
2026-01-10 13:50:24 +0900

    Show computer choice each round

--- script.js
+++ script.js
@@ -41,27 +41,36 @@ let humanScore = 0;
 function playGame() {
         function playRound (humanChoice, computerChoice) {
         if (humanChoice === 'rock' && computerChoice === 'paper') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('You lose! Paper beats Rock');
             computerScore++;
         } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('You win! Rock beats scissors');
             humanScore++;
         } else if (humanChoice === 'rock' && computerChoice === 'rock') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('It\'s a tie');
         } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('It\'s a tie');
         } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('You lose! Scissors beat Paper');
             computerScore++;
         } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('You win! Paper beats rock');
             humanScore++;
         } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('You win! Scissors beat Paper');
             humanScore++;
         } else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('It\'s a tie');
         } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            console.log('Computer chose: ' + computerChoice);
             console.log('You lose! Rock beats Scissors');
             computerScore++;
         } 
